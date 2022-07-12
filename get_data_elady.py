ff32e0118a6e254d8237589fba5d97b644f4ed80
kremeth <66464637+kremeth@users.noreply.github.com>
2022-07-12 16:03:23 +0800

    Add files via upload

---
+++ get_data_elady.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import glob
+import os
+import numpy as np
+import requests
+import io
+import flask
+
+
+app = flask.Flask(__name__)
+# Get the csv
+r = requests.get('https://dropshipping.elady.com/dropshipping/api_item.php?login=drapi4749sb&password=vtx9nE7VnHcP')
+
+# Turn the content into a pandas dataframe
+data = pd.read_csv(io.StringIO(r.content.decode('utf-8')))
+
+# Filter out the items that are not BAGS
+data = data[data['category'] == 'BAGS']
+data.reset_index(inplace=True, drop=True)
+
+# Get the first 50 items
+@app.route('/')
+@app.route('/home')
+def home():
+    return 'waiting'
+
+@app.route('/50')
+def fifty():
+    return data.iloc[:50, :]
+
+if __name__ == "__main__":
+    app.secret_key = 'ItIsASecret'
+    app.debug = True
+    app.run()
