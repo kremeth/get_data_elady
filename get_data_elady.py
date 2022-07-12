45177a272c85474218fa11052482617fb25404af
kremeth <66464637+kremeth@users.noreply.github.com>
2022-07-12 17:46:13 +0800

    Update get_data_elady.py

--- get_data_elady.py
+++ get_data_elady.py
@@ -28,7 +28,7 @@ def fifty():
     # Filter out the items that are not BAGS
     data = data[data['category'] == 'BAGS']
     data.reset_index(inplace=True, drop=True)
-    return data.iloc[0, :].sku
+    return data.to_json(orient="records")
 
 if __name__ == "__main__":
     app.secret_key = 'ItIsASecret'
