92ad302d7c24737fbfa483dd7d47559add937ae5
kremeth <66464637+kremeth@users.noreply.github.com>
2022-07-12 17:13:34 +0800

    Update get_data_elady.py

--- get_data_elady.py
+++ get_data_elady.py
@@ -7,11 +7,7 @@ import flask
 
 
 app = flask.Flask(__name__)
-# Get the csv
-r = requests.get('https://dropshipping.elady.com/dropshipping/api_item.php?login=drapi4749sb&password=vtx9nE7VnHcP')
 
-# Turn the content into a pandas dataframe
-data = pd.read_csv(io.StringIO(r.content.decode('utf-8')))
 
 # Filter out the items that are not BAGS
 data = data[data['category'] == 'BAGS']
@@ -25,6 +21,11 @@ def home():
 
 @app.route('/50')
 def fifty():
+    # Get the csv
+    r = requests.get('https://dropshipping.elady.com/dropshipping/api_item.php?login=drapi4749sb&password=vtx9nE7VnHcP')
+
+    # Turn the content into a pandas dataframe
+    data = pd.read_csv(io.StringIO(r.content.decode('utf-8')))
     return data.iloc[:50, :]
 
 if __name__ == "__main__":
